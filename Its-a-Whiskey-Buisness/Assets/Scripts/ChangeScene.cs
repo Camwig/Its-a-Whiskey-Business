104c7a5d4db021d2f510190f840e99a1b0ffd03a
mattgebara <2001066@uad.ac.uk>
2023-04-19 13:19:50 +0100

    Working some more to finish
    It is me Cameron writing this harhar

--- Its-a-Whiskey-Buisness/Assets/Scripts/ChangeScene.cs
+++ Its-a-Whiskey-Buisness/Assets/Scripts/ChangeScene.cs
@@ -6,54 +6,22 @@ using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour
 {
 
-    //public Animator transition;
-
-    //public float transitionTime = 0f;
-
-   // public LevelLoader new_loader;
-
-    //public int SceneID;
-
     public GameObject activatingcam;
     public GameObject deactivatingcam;
-
-    //public GameObject activatingpause;
-    //public GameObject deactivatingpause;
-
-    //public GameObject activatingRoom;
-    //public GameObject deactivatingRoom;
 
 
 
     public void ActivatingScene()
     { 
-        //StartCoroutine(LoadLevel());
-      //  activatingRoom.SetActive(true);
-      //  activatingpause.SetActive(true);
+
         activatingcam.SetActive(true);
     }
 
     public void DeactivatingScene()
     {
-     //   deactivatingRoom.SetActive(false);
-    //    deactivatingpause.SetActive(false);
         deactivatingcam.SetActive(false);
     }
-
-
-       //  activatingcam.SetActive(true);
-       //  deactivatingcam.SetActive(false);
-
-       //// activatingRoom.SetActive(true);
-
-
-       // //if (activatingRoom == true)
-       //// {
-       //     activatingpause.SetActive(true);
-       ////     deactivatingRoom.SetActive(false);
-       //     deactivatingpause.SetActive(false);
-       //// }
-       
+     
     
 
 }
