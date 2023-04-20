d63e26fb570c9c54250315899ab9923489407afb
mattgebara <2001066@uad.ac.uk>
2023-04-20 18:05:51 +0100

    Revert "to remove"
    This reverts commit ac31ca9b3805bb8a9f67fdaedf8e7d10423f1f3b.

--- Its-a-Whiskey-Buisness/Assets/Scripts/ChangePicture.cs
+++ Its-a-Whiskey-Buisness/Assets/Scripts/ChangePicture.cs
@@ -4,112 +4,36 @@ using UnityEngine;
 
 public class ChangePicture : MonoBehaviour
 {
-    public GameObject image1;
-    public GameObject image2;
-    public GameObject image3;
-    public GameObject image4;
-    public GameObject image5;
-    public GameObject image6;
-    public GameObject image7;
-    public GameObject image8;
-    public GameObject Button;
-
-    public GameObject currentImage;
-    public GameObject NextImage;
-
-    public int Imagenum;
-
-    public Animator Changeimage;
-
-    private int clickCount = 0;
+    public GameObject activatingimage;
+    public GameObject deactivatingimage;
 
 
-    void Update()
+
+    public void MoveToScene()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            IfButtonClicked();
-        }
+        StartCoroutine(LoadLevel());
     }
 
-    private void Start()
+    IEnumerator LoadLevel()
     {
-        Button.SetActive(false);
+        yield return new WaitForSeconds(0);
 
-       StartCoroutine(OpenButton());
-        
-    }
-    IEnumerator OpenButton()
-    {
-     yield return new WaitForSeconds(3f);
-
-     Button.SetActive(true);
-    }
-    public void IfButtonClicked()
-    {
-         clickCount++;
-
-        Button.SetActive(false);
-        StartCoroutine(ChangeImage());
-
-        if (clickCount > 7)
-        {
-           Button.SetActive(false);
-            clickCount = 0;
-            return;
-        }      
-    }
-
-    IEnumerator ChangeImage()
-    {
-        Changeimage.SetTrigger("Start");
-        yield return new WaitForSeconds(3f);
-        Imagenum = Imagenum + 1;
-        Button.SetActive(true);
-    }
-
-    public void ActivateImage2()
-    {
-            image1.SetActive(false);
+        activatingimage.SetActive(true);
+        deactivatingimage.SetActive(true);
 
     }
-    public void ActivateImage3()
-    {
-  
-            image2.SetActive(false);
- 
-    }
-    public void ActivateImage4()
+
+
+    public void ActivatingScene()
     {
 
-            image3.SetActive(false);
- 
-
-    }
-    public void ActivateImage5()
-    {
-            image4.SetActive(false);
-
-    }
-    public void ActivateImage6()
-    {
-            image5.SetActive(false);
-
-    }
-    public void ActivateImage7()
-    {
-            image6.SetActive(false);
-    }
-    public void ActivateImage8()
-    {
-       
-            image7.SetActive(false);
-        
+        activatingimage.SetActive(true);
     }
 
     public void DeactivatingScene()
     {
-        currentImage.SetActive(false);
+        deactivatingimage.SetActive(false);
     }
+
 
 }
