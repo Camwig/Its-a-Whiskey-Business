12665ba71db6974c3a6af61cc074e02802ccabef
mattgebara <2001066@uad.ac.uk>
2023-04-25 17:53:53 +0100

    hopefully that is the room wise things fixed

--- Its-a-Whiskey-Buisness/Assets/PrefabRoom/ActivatingLayers.cs
+++ Its-a-Whiskey-Buisness/Assets/PrefabRoom/ActivatingLayers.cs
@@ -12,53 +12,52 @@ public class ActivatingLayers : MonoBehaviour
 
     public void ActivateRoom1()
     {
-
-        Room1.SetActive(true);
+        
         Room2.SetActive(false);
         Room3.SetActive(false);
         Room4.SetActive(false);
         Email.SetActive(false);
+        Room1.SetActive(true);
 
     }
     public void ActivateRoom2()
     {
 
-        Room2.SetActive(true);
+       
         Room1.SetActive(false);
         Room3.SetActive(false);
         Room4.SetActive(false);
         Email.SetActive(false);
+        Room2.SetActive(true);
 
     }
     public void ActivateRoom3()
     {
 
-        Room3.SetActive(true);
+        
         Room2.SetActive(false);
         Room1.SetActive(false);
         Room4.SetActive(false);
         Email.SetActive(false);
-
+        Room3.SetActive(true);
     }
     public void ActivateRoom4()
     {
-
-        Room4.SetActive(true);
-        Room2.SetActive(false);
-        Room3.SetActive(false);
+        
         Room1.SetActive(false);
+        Room3.SetActive(false);
+        Room2.SetActive(false);
         Email.SetActive(false);
+        Room4.SetActive(true);
 
     }
     public void ActivateEmail()
     {
-
-        Email.SetActive(true);
         Room1.SetActive(false);
         Room2.SetActive(false);
         Room3.SetActive(false);
         Room4.SetActive(false);
-
+        Email.SetActive(true);
     }
 
     public void Reset()
@@ -69,7 +68,6 @@ public class ActivatingLayers : MonoBehaviour
         Room3.SetActive(true);
         Room4.SetActive(true);
         Email.SetActive(true);
-
     }
 
 }
